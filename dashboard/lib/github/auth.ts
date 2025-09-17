0bbdc9033813146d732bc89cce0b29fcf17a1366
Gradual Systems <support@gradualsystems.io>
2025-09-17 08:58:45 -0700

    fix: syncUserRepo

--- dashboard/lib/github/auth.ts
+++ dashboard/lib/github/auth.ts
@@ -261,7 +261,7 @@ export async function syncUserRepositories(
   const { error } = await supabase
     .from('repositories')
     .upsert(repoData, {
-      onConflict: 'idx_repositories_github_repo',
+      onConflict: 'installation_id,github_repo_id',
     })
     .select();
 
